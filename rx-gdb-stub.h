30e800a69f9e58ad751ce6afa438b6d8f9729a62
Maxim Salov <max.salov@gmail.com>
2013-08-04 22:37:17 +0300

    Prevent mangling of function names (if C++ is used)

--- rx-gdb-stub.h
+++ rx-gdb-stub.h
@@ -1,7 +1,15 @@
 #ifndef RX_GDB_STUB_H__
 #define RX_GDB_STUB_H__
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void debug_puts (const char *str);
 void stub_init (void);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
 
 #endif /* RX_GDB_STUB_H__ */
